ff757ae89dc8bd1dd493a75826afa00c1535a2e8
minsukim1 <58473738+minsukim1@users.noreply.github.com>
2020-03-10 09:52:16 +0900

    20.03.10
    jhta study

---
+++ Projects/java_workspace/basic/src/datatype/DataTypeDemo2.java
@@ -0,0 +1,19 @@
+package datatype;
+
+public class DataTypeDemo2 {
+
+	public static void main(String[] args) {
+		
+		// 변수의 선언과 초기화
+		// 자료형 식별자 = 값;
+		
+		// int타입의 변수(저장소)를 만들고, 80을 대입한다.
+		int kor = 80;
+		int math = 70;
+		int eng = 70;
+		
+		System.out.println(kor + eng + math);
+		System.out.println((kor + eng + math)/3);
+	}
+
+}
