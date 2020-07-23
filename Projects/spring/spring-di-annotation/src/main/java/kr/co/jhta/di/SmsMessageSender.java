26f47e03e1eb3aecc520671579ca6862e04a727f
msusuKim <kwsoj123@naver.com>
2020-07-23 17:24:16 +0900

    2020-07-23[Spring]

---
+++ Projects/spring/spring-di-annotation/src/main/java/kr/co/jhta/di/SmsMessageSender.java
@@ -0,0 +1,25 @@
+package kr.co.jhta.di;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("sms")
+public class SmsMessageSender implements MessageSender {
+	@Value("${message.sender.sms.company}")
+	private String company;
+	
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	@Override
+	public void send(String from, String to, String subject, String content) {
+		System.out.println(company+"통신사를 이용합니다");
+		System.out.println("발신자 : "+ from);
+		System.out.println("수신자 : "+ to);
+		System.out.println("제목 : "+ subject);
+		System.out.println("내용 : "+ content);
+		System.out.println("------------------------------------");
+		
+	}
+}
